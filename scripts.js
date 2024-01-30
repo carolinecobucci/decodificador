eac760ad7d51a28ef4e216d7d508b2068c684ab4
Caroline Cobucci <cacobuti@gmail.com>
2024-01-30 14:28:08 -0300

    added validations

--- scripts.js
+++ scripts.js
@@ -28,6 +28,27 @@ function decrypt(text, shift) {
     .join("");
 }
 
+// Função para validar o texto de entrada
+function validateInputText(text) {
+  const regex = /^[a-z]+$/;
+
+  // Verifica se há caracteres maiúsculos ou especiais na string
+  if (!regex.test(text)) {
+    return false;
+  }
+
+  // Verifica individualmente se cada caractere é minúsculo
+  for (let i = 0; i < text.length; i++) {
+    const charCode = text.charCodeAt(i);
+    // Considera apenas caracteres ASCII que não são letras minúsculas
+    if (charCode < 97 || charCode > 122) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 // Função para copiar texto para a área de transferência usando a API Clipboard
 async function copyToClipboard(text) {
   try {
@@ -45,15 +66,31 @@ document.addEventListener("DOMContentLoaded", () => {
   const descriptoBtn = document.getElementById("descripto-btn");
   const outputText = document.getElementById("output-text");
   const copyBtn = document.getElementById("copy-btn");
+  const infoDiv = document.querySelector(".info");
+  const outputOffDiv = document.querySelector(".output-off");
+  const outputOnDiv = document.querySelector(".output-on");
 
   criptoBtn.addEventListener("click", () => {
-    const shiftedText = encrypt(inputText.value, 3);
-    outputText.value = shiftedText;
+    const isValid = validateInputText(inputText.value);
+
+    if (isValid) {
+      const shiftedText = encrypt(inputText.value, 3);
+      outputText.value = shiftedText;
+
+      outputOnDiv.style.display = "block";
+      outputOffDiv.style.display = "none";
+      infoDiv.style.display = "none";
+    } else {
+      infoDiv.style.display = "block";
+    }
   });
 
   descriptoBtn.addEventListener("click", () => {
     const originalText = decrypt(outputText.value, 3);
     outputText.value = originalText;
+
+    outputOnDiv.style.display = "block";
+    outputOffDiv.style.display = "none";
   });
 
   copyBtn.addEventListener("click", () => {
